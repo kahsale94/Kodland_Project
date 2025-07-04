f9e8e60526605411053734e9793d9bbbe61e6e4a
Kaiky <ultimateclash22@gmail.com>
2025-07-03 22:26:50 -0300

    Finalmente deu certo

--- Python/main.py
+++ Python/main.py
@@ -1,13 +1,37 @@
 import pgzrun
 
+alien = Actor('alien')
+alien.topright = 0, 250
 
 WIDTH = 800
-HEIGHT = 600
-
+HEIGHT = 500
+TITLE = "Roguelike Adventure"
 
 def draw():
     screen.clear()
+    alien.draw()
     screen.draw.circle((400, 300), 30, 'blue')
 
+def update():
+    alien.left += 2
+    if alien.left > WIDTH:
+        alien.right = 0
+    on_touch_circle()
+
+def on_touch_circle():
+    if alien.left == 350:
+        set_alien_hurt()
+
+def on_mouse_down(pos):
+    if alien.collidepoint(pos):
+        set_alien_hurt()
+
+def set_alien_hurt():
+    alien.image = 'alien_hurt'
+    sounds.loli.play()
+    clock.schedule_unique(set_alien_normal, 1.0)
+
+def set_alien_normal():
+    alien.image = 'alien'
 
 pgzrun.go()
